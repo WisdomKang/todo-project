e192173804877f8ced8cdbc18f46e622552f8916
WisdomKang <buckgusaud@gmail.com>
2024-12-19 15:36:50 +0900

    create : database init

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
 
 func main() {
 	fmt.Println("test hello!")
+
+	dns := "host=localhost user=root password=todo5432 dbname=gorm port=15432 sslmode=disable TimeZone=Asia/Seoul"
+
+	db, err := gorm.Open(postgres.Open(dns), &gorm.Config{})
+	if err != nil {
+		panic("Db 연결에 실패하였습니다.")
+	}
+
+	db.AutoMigrate(&Test{})
+}
+
+type Test struct {
+	gorm.Model
+
+	Name string
+	Agwe int
 }
